06e3728809463e9357b6a8b65a91e473f4b0d11e
benizar <benizar@gmail.com>
2026-06-10 06:07:09 +0200

    Improve unaltredocs download support
    Add reusable download cards, detail pages, and documentation styling so downstream docs can publish platform-specific install flows.

--- _plugins/theme-cache-bust.rb
+++ _plugins/theme-cache-bust.rb
@@ -10,6 +10,8 @@ module Jekyll
       private
 
       def directory_files_content
+        return main_css_source_content if directory == "assets/_sass"
+
         target_directory = existing_path(directory)
         return "" unless target_directory
 
@@ -20,16 +22,34 @@ module Jekyll
 
       def file_content
         local_file_name = file_name.slice((file_name.index("assets/")..-1))
+        return main_css_source_content if local_file_name == "assets/css/main.css"
+
         target_file = existing_path(local_file_name)
         return "" unless target_file
 
         File.read(target_file)
       end
 
+      def main_css_source_content
+        files = existing_paths("assets/css/main.scss")
+        existing_paths("_sass").each do |sass_dir|
+          files.concat(Dir[File.join(sass_dir, "**", "*")].reject { |file| File.directory?(file) })
+        end
+        files.uniq.sort.map { |file| File.read(file) }.join
+      end
+
       def existing_path(relative_path)
         return nil if relative_path.nil? || relative_path.empty?
 
-        [relative_path, File.join(UNALTRAWEB_ROOT, relative_path)].find { |path| File.exist?(path) }
+        existing_paths(relative_path).first
+      end
+
+      def existing_paths(relative_path)
+        [
+          relative_path,
+          File.join("docs", relative_path),
+          File.join(UNALTRAWEB_ROOT, relative_path),
+        ].select { |path| File.exist?(path) }
       end
     end
   end
